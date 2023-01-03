1f6f89bfebf0dfda7a80f12b79fc678eb4298f2d
daewon-ko <eodnjs568@naver.com>
2023-01-03 19:57:02 +0900

    Solving Quetion 7

--- src/do_it_algorithm/Q7.java
+++ src/do_it_algorithm/Q7.java
@@ -1,27 +1,25 @@
+package do_it_algorithm;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Q7 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bufferedReader.readLine());
         int M = Integer.parseInt(bufferedReader.readLine());
-
+        int [] A = new int[N];
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int [] A = new int [N];
-
         for(int i =0; i<N; i++){
             A[i] = Integer.parseInt(stringTokenizer.nextToken());
         }
         Arrays.sort(A);
+        int count = 0;
         int i = 0;
         int j = N-1;
-        int count = 0;
-
         while (i<j){
             if(A[i]+A[j]<M){
                 i++;
@@ -35,7 +33,6 @@ public class Main {
         }
         System.out.println(count);
         bufferedReader.close();
-
 
 
     }
